bf1bf1aa7e760b2f26d7095bd2f8747e96b2aa01
William Zhou <103528449+WLZ0819skool@users.noreply.github.com>
2022-05-17 17:57:15 +0000

    done!

--- part4.py
+++ part4.py
@@ -12,4 +12,6 @@ Define a function possibletriangle that takes 3 arguments (side1, side2, side3).
 '''
 
 def possibletriangle(side1, side2, side3): #do not change this line
-  pass #delete the word pass when you start writing your code
+  mylist = [side1,side2,side3]
+  mylist.sort()
+  return mylist[0] + mylist[1] > mylist[2]
